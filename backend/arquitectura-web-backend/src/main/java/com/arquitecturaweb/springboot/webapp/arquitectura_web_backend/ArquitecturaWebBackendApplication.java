cc24f0da75c3cefdf8a7ba61ac54a224e1b6dcf2
juanrey <juanrey@deloitte.com>
2024-11-07 20:22:19 -0300

    spring boot backend init

---
+++ backend/arquitectura-web-backend/src/main/java/com/arquitecturaweb/springboot/webapp/arquitectura_web_backend/ArquitecturaWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arquitecturaweb.springboot.webapp.arquitectura_web_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturaWebBackendApplication.class, args);
+	}
+
+}
---
+++ backend/arquitectura-web-backend/src/test/java/com/arquitecturaweb/springboot/webapp/arquitectura_web_backend/ArquitecturaWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitecturaweb.springboot.webapp.arquitectura_web_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
